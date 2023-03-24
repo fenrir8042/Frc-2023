0b83650601f96ee81355ac57e53a09cf00168e49
Fenrir Robotics <124340945+fenrir8042@users.noreply.github.com>
2023-03-24 16:42:51 +0300

    friday work 2
    libraries fixed.

--- thor-Imported/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ thor-Imported/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,8 +4,9 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax; 
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.MotControllerJNI;
